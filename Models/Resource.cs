6977a906871e2e99ec990f4a0f9867e4afc597a0
Misokuhle <misokuhle46@gmail.com>
2025-07-16 20:44:49 +0200

    Updated resource name validation

--- Models/Resource.cs
+++ Models/Resource.cs
@@ -8,12 +8,14 @@ namespace InternalResourceBookingSystem.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Resource Name")]
         public string Name { get; set; }
 
         public string? Description { get; set; }
 
         public string? Location { get; set; }
 
+        [Required(ErrorMessage = "Capacity is required")]
         [Range(1, int.MaxValue, ErrorMessage = "Capacity must be a positive number")]
         public int Capacity {  get; set; }
 
