d5a65844c022d44973566de6adf618a7d69f9f6a
Misokuhle <misokuhle46@gmail.com>
2025-07-16 09:10:35 +0200

    Initial commit with .gitignore

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InternalResourceBookingSystem.Models;
+
+namespace InternalResourceBookingSystem.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        
+        }
+
+        //DbSets for Booking and Resource
+    }
+}
